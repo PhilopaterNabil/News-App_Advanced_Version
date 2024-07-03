0e17b0d871ce6d563b7a816595971dd38af00b85
fady emil <fady46t45f@gmail.com>
2024-07-03 11:29:44 +0300

    sort files

---
+++ lib/widgets/Sign_UP_Widget/Sign_UPFiled.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sin_api/cubit/user_cubit.dart';
+import 'package:sin_api/cubit/user_state.dart';
+import 'package:sin_api/widgets/Sign_UP_Widget/already_have_an_account_widget.dart';
+import 'package:sin_api/widgets/App/custom_form_button.dart';
+import 'package:sin_api/widgets/App/custom_input_field.dart';
+import 'package:sin_api/widgets/App/page_header.dart';
+import 'package:sin_api/widgets/App/page_heading.dart';
+import 'package:sin_api/widgets/Sign_UP_Widget/pick_image_widget.dart';
+
+class SignUpFiled extends StatelessWidget {
+  const SignUpFiled({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<UserCubit, UserState>(listener: (context, state) {
+      if (state is SignUpSuccess) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(state.message),
+        ));
+      } else if (state is SignUpFailure) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(state.errMessage),
+        ));
+      }
+    }, builder: (context, state) {
+      return Column(
+        children: <Widget>[
+          const PageHeader(),
+          const PageHeading(title: 'Sign-up'),
+          //! Image
+          const PickImageWidget(),
+          const SizedBox(height: 16),
+          //! Name
+          CustomInputField(
+            labelText: 'Name',
+            hintText: 'Your name',
+            isDense: true,
+            controller: context.read<UserCubit>().signUpName,
+          ),
+          const SizedBox(height: 16),
+          //!Email
+          CustomInputField(
+            labelText: 'Email',
+            hintText: 'Your email',
+            isDense: true,
+            controller: context.read<UserCubit>().signUpEmail,
+          ),
+          const SizedBox(height: 16),
+          //! Phone Number
+          CustomInputField(
+            labelText: 'Phone number',
+            hintText: 'Your phone number ex:01234567890',
+            isDense: true,
+            controller: context.read<UserCubit>().signUpPhoneNumber,
+          ),
+          const SizedBox(height: 16),
+          //! Password
+          CustomInputField(
+            labelText: 'Password',
+            hintText: 'Your password',
+            isDense: true,
+            obscureText: true,
+            suffixIcon: true,
+            controller: context.read<UserCubit>().signUpPassword,
+          ),
+          //! Confirm Password
+          CustomInputField(
+            labelText: 'Confirm Password',
+            hintText: 'Confirm Your password',
+            isDense: true,
+            obscureText: true,
+            suffixIcon: true,
+            controller: context.read<UserCubit>().confirmPassword,
+          ),
+          const SizedBox(height: 22),
+          //!Sign Up Button
+          if (state is SignUpLoading)
+            const CircularProgressIndicator()
+          else
+            CustomFormButton(
+              innerText: 'Signup',
+              onPressed: () {
+                context.read<UserCubit>().signUp();
+              },
+            ),
+          const SizedBox(height: 18),
+          //! Already have an account widget
+          const AlreadyHaveAnAccountWidget(),
+          const SizedBox(height: 30),
+        ],
+      );
+    },);
+  }
+}
---
+++ lib/widgets/Sign_inWidget/Sign_in_Filed.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sin_api/cubit/user_cubit.dart';
+import 'package:sin_api/cubit/user_state.dart';
+import 'package:sin_api/screens/profile_screen.dart';
+import 'package:sin_api/widgets/App/custom_form_button.dart';
+import 'package:sin_api/widgets/App/custom_input_field.dart';
+import 'package:sin_api/widgets/Sign_inWidget/dont_have_an_account.dart';
+import 'package:sin_api/widgets/Sign_inWidget/forget_password_widget.dart';
+import 'package:sin_api/widgets/App/page_header.dart';
+import 'package:sin_api/widgets/App/page_heading.dart';
+
+class Sign_in_Filed extends StatelessWidget {
+  const Sign_in_Filed({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<UserCubit, UserState>(listener: (context, state) {
+      if (state is SignInSuccess) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("success"),
+          ),
+        );
+        // context.read<UserCubit>().getUserProfile();
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ProfileScreen(),
+          ),
+        );
+      } else if (state is SignInFailure) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('opps error enter email , password  reset'),
+          ),
+        );
+      }
+    }, builder: (context, state) {
+      return Column(
+        children: [
+          const PageHeader(),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: context.read<UserCubit>().signInFormKey,
+                  child: Column(
+                    children: [
+                      const PageHeading(title: 'Sign-in'),
+                      //!Email
+                      CustomInputField(
+                        labelText: 'Email',
+                        hintText: 'Your email',
+                        controller: context.read<UserCubit>().signInEmail,
+                      ),
+                      const SizedBox(height: 16),
+                      //!Password
+                      CustomInputField(
+                        labelText: 'Password',
+                        hintText: 'Your password',
+                        obscureText: true,
+                        suffixIcon: true,
+                        controller: context.read<UserCubit>().signInPassword,
+                      ),
+                      const SizedBox(height: 16),
+                      //! Forget password?
+                      ForgetPasswordWidget(size: size),
+                      const SizedBox(height: 20),
+                      //!Sign In Button
+                      if (state is SignInLoading)
+                        const CircularProgressIndicator()
+                      else
+                        CustomFormButton(
+                          innerText: 'Sign In',
+                          onPressed: () {
+                            context.read<UserCubit>().signing();
+                          },
+                        ),
+                      const SizedBox(height: 18),
+                      //! Dont Have An Account ?
+                      DontHaveAnAccountWidget(size: size),
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+    });
+  }
+}
